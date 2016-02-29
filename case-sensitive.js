90c030e5bfefb22b944c17e122acfd1fb6f54658
Andres Suarez <zertosh@gmail.com>
2016-02-29 11:36:37 -0800

    flesh out case-sensitive

--- case-sensitive.js
+++ case-sensitive.js
@@ -1,9 +1,63 @@
 'use strict';
 
-// TODO: check require.resolve;
+var commondir = require('commondir');
+var path = require('path');
+var resolve = require('./lib/resolve');
+var fs = require('fs');
+
+var externalRe = /^[^./]/;
 
 module.exports = function(context) {
-  return {};
+
+  var target = context.getFilename();
+  var basedir = path.dirname(target);
+
+  function validate(node, value) {
+    if (externalRe.test(value)) return;
+    var resolved = resolve.sync(value, {basedir: basedir});
+    if (!resolved) return;
+    var prefix = commondir([target, resolved]);
+
+  }
+
+  return {
+    CallExpression: function(node) {
+      // require('dep')
+      if (
+        node.callee.type === 'Identifier' &&
+        node.callee.name === 'require' &&
+        node.arguments[0] &&
+        node.arguments[0].type === 'Literal'
+      ) {
+        validate(node, node.arguments[0].value);
+        return;
+      }
+      // require.resolve('dep')
+      if (
+        node.callee.type === 'MemberExpression' &&
+        node.callee.computed === false &&
+        node.callee.object.type === 'Identifier' &&
+        node.callee.object.name === 'require' &&
+        node.callee.property.type === 'Identifier' &&
+        node.callee.property.name === 'resolve' &&
+        node.arguments[0] &&
+        node.arguments[0].type === 'Literal'
+      ) {
+        validate(node, node.arguments[0].value);
+        return;
+      }
+    },
+    ImportDeclaration: function(node) {
+      validate(node, node.source.value);
+    },
+  };
 };
 
 module.exports.schema = [];
+
+var readdirCache = Object.create(null);
+
+function readdirSync(dirname) {
+  // if (readdirCache[dirname])
+  fs.readdirSync(dirname);
+}
