d88b5abead02bc576f7ec5c5deafde90f73efe39
Andres Suarez <zertosh@gmail.com>
2016-05-08 16:16:35 -0700

    finish case-sensitive

--- case-sensitive.js
+++ case-sensitive.js
@@ -6,35 +6,114 @@ var fs = require('fs');
 
 var resolveSync = require('./_resolveSync');
 
-var externalRe = /^[^./]/;
+var nodeExts = /\.(js|json|node)$/;
+
+var readdirCache = Object.create(null);
+function readdirSync(dirname) {
+  if (!(dirname in readdirCache)) {
+    try {
+      readdirCache[dirname] = fs.readdirSync(dirname);
+    } catch (err) {
+      readdirCache[dirname] = null;
+    }
+  }
+  return readdirCache[dirname];
+}
+
+// turns "/a/b/c.js" into ["/a", "/a/b", "/a/b/c.js"]
+function pathSteps(pathString) {
+  return pathString
+    .split('/')
+    .map(function(part, i, parts) {
+      return parts.slice(0, i + 1).join('/');
+    })
+    .filter(Boolean);
+}
+
+// if more than one possible suggestion is found, return none.
+function getCaseSuggestion(needle, haystack) {
+  var found = false;
+  var lneedle = needle.toLowerCase();
+  for (var i = 0; i < haystack.length; i++) {
+    if (lneedle === haystack[i].toLowerCase()) {
+      if (found) return false;
+      found = haystack[i];
+    }
+  }
+  return found;
+}
 
 module.exports = function(context) {
-
   var target = context.getFilename();
-  var basedir = path.dirname(target);
 
-  function validate(node, value) {
-    if (externalRe.test(value)) return;
-    var resolved = resolveSync(value, {basedir: basedir});
+  var resolveOpts = {
+    basedir: path.dirname(target),
+    extensions: ['.js', '.json', '.node'],
+  };
+
+  function validate(node, valueNode) {
+    var value = valueNode.value;
+    var resolved = resolveSync(value, resolveOpts);
     if (!resolved) return;
     var prefix = commondir([target, resolved]);
+    pathSteps(resolved)
+      .filter(function(step) {
+        // remove the steps outside of our request
+        return step.indexOf(prefix) !== -1;
+      })
+      .forEach(function(step, i, steps) {
+        var basename = path.basename(step);
+        var dirname = path.dirname(step);
+        var dirlist = readdirSync(dirname);
 
+        // compare the directory listing to the requested path. this works
+        // because "resolve" resolves by concating the path segments from the
+        // input, so the resolved path will have the incorrect case:
+        if (dirlist.indexOf(basename) !== -1) return;
+
+        var shouldRemoveExt =
+          i === steps.length - 1 &&   // last step
+          nodeExts.test(basename) &&  // expected
+          !nodeExts.test(value);      // actual
+
+        var suggestion = getCaseSuggestion(basename, dirlist);
+
+        var incorrect = shouldRemoveExt
+          ? basename.replace(nodeExts, '')
+          : basename;
+
+        var correct = shouldRemoveExt && suggestion
+          ? suggestion.replace(nodeExts, '')
+          : suggestion;
+
+        if (correct) {
+          context.report({
+            node: valueNode,
+            data: {incorrect: incorrect, correct: correct},
+            message: 'Case mismatch in "{{incorrect}}", expected "{{correct}}".',
+          });
+        } else {
+          context.report({
+            node: valueNode,
+            data: {incorrect: incorrect},
+            message: 'Case mismatch in "{{incorrect}}".',
+          });
+        }
+      });
   }
 
   return {
     CallExpression: function(node) {
-      // require('dep')
       if (
+        // require("…");
         node.callee.type === 'Identifier' &&
         node.callee.name === 'require' &&
         node.arguments[0] &&
         node.arguments[0].type === 'Literal'
       ) {
-        validate(node, node.arguments[0].value);
-        return;
-      }
-      // require.resolve('dep')
-      if (
+        validate(node, node.arguments[0]);
+      } else if (
+        // require.resolve("…");
         node.callee.type === 'MemberExpression' &&
         node.callee.computed === false &&
         node.callee.object.type === 'Identifier' &&
@@ -44,21 +123,27 @@ module.exports = function(context) {
         node.arguments[0] &&
         node.arguments[0].type === 'Literal'
       ) {
-        validate(node, node.arguments[0].value);
-        return;
+        validate(node, node.arguments[0]);
       }
     },
     ImportDeclaration: function(node) {
-      validate(node, node.source.value);
+      // import … from "…";
+      // import "…";
+      validate(node, node.source);
+    },
+    ExportAllDeclaration: function(node) {
+      // export * from "…";
+      if (node.source && node.source.type === 'Literal') {
+        validate(node, node.source);
+      }
+    },
+    ExportNamedDeclaration: function(node) {
+      // export … from "…";
+      if (node.source && node.source.type === 'Literal') {
+        validate(node, node.source);
+      }
     },
   };
 };
 
 module.exports.schema = [];
-
-var readdirCache = Object.create(null);
-
-function readdirSync(dirname) {
-  // if (readdirCache[dirname])
-  fs.readdirSync(dirname);
-}
--- test/case-sensitive-test.js
+++ test/case-sensitive-test.js
@@ -1,1 +1,250 @@
 'use strict';
+
+var RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('case-sensitive', require.resolve('../case-sensitive'), {
+  valid: [
+    // require(…);
+    {
+      filename: __filename,
+      code: 'require("../index")',
+    },
+    {
+      filename: __filename,
+      code: 'require("..")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/foo")',
+    },
+    {
+      filename: __filename,
+      code: 'require("resolve")',
+    },
+    {
+      filename: __filename,
+      code: 'require("non-existing-filename")',
+    },
+    {
+      filename: __filename,
+      code: 'require(foo)',
+    },
+    // require.resolve(…);
+    {
+      filename: __filename,
+      code: 'require.resolve("../index")',
+    },
+    // import "…";
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import "../index"',
+    },
+    // import … from …;
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import x from "../index"',
+    },
+    // export * from "…";
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export {foo} from "../index"',
+    },
+    // export … from "…";
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export * from "../index"',
+    },
+  ],
+  invalid: [
+    //
+    // parent dir
+    //
+    {
+      filename: __filename,
+      code: 'require("../Index")',
+      errors: [
+        'Case mismatch in "Index", expected "index".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("../Index.js")',
+      errors: [
+        'Case mismatch in "Index.js", expected "index.js".',
+      ],
+    },
+
+    //
+    // nested dir
+    //
+    {
+      filename: __filename,
+      code: 'require("./Case-Sensitive/Bar/baz")',
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/bar/baz")',
+      errors: [
+        'Case mismatch in "bar", expected "Bar".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Bar/bAz")',
+      errors: [
+        'Case mismatch in "bAz", expected "baz".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Bar/Baz.js")',
+      errors: [
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./Case-Sensitive/bar/Baz.js")',
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+
+    //
+    // node_modules
+    //
+    {
+      filename: __filename,
+      code: 'require("Resolve")',
+      errors: [
+        'Case mismatch in "Resolve", expected "resolve".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("Resolve/index")',
+      errors: [
+        'Case mismatch in "Resolve", expected "resolve".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("Resolve/Index")',
+      errors: [
+        'Case mismatch in "Resolve", expected "resolve".',
+        'Case mismatch in "Index", expected "index".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("Resolve/Index.js")',
+      errors: [
+        'Case mismatch in "Resolve", expected "resolve".',
+        'Case mismatch in "Index.js", expected "index.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("resolve/Index")',
+      errors: [
+        'Case mismatch in "Index", expected "index".',
+      ],
+    },
+
+    //
+    // require.resolve, import, export
+    //
+    {
+      filename: __filename,
+      code: 'require.resolve("./Case-Sensitive/bar/Baz.js")',
+      parserOptions: {sourceType: 'module'},
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'import "./Case-Sensitive/bar/Baz.js"',
+      parserOptions: {sourceType: 'module'},
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'import baz from "./Case-Sensitive/bar/Baz.js"',
+      parserOptions: {sourceType: 'module'},
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'export * from "./Case-Sensitive/bar/Baz.js"',
+      parserOptions: {sourceType: 'module'},
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'export {baz} from "./Case-Sensitive/bar/Baz.js"',
+      parserOptions: {sourceType: 'module'},
+      errors: [
+        'Case mismatch in "Case-Sensitive", expected "case-sensitive".',
+        'Case mismatch in "bar", expected "Bar".',
+        'Case mismatch in "Baz.js", expected "baz.js".',
+      ],
+    },
+
+    //
+    // .json, .node
+    //
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Quux")',
+      errors: [
+        'Case mismatch in "Quux", expected "quux".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Quux.node")',
+      errors: [
+        'Case mismatch in "Quux.node", expected "quux.node".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Qux")',
+      errors: [
+        'Case mismatch in "Qux", expected "qux".',
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./case-sensitive/Qux.json")',
+      errors: [
+        'Case mismatch in "Qux.json", expected "qux.json".',
+      ],
+    },
+  ],
+});
