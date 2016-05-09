7c2452c0f2e06f4ebc28cee8ce05d1ab249f82a4
Andres Suarez <zertosh@gmail.com>
2016-05-08 17:45:08 -0700

    finish no-unresolved

--- no-unresolved.js
+++ no-unresolved.js
@@ -1,9 +1,51 @@
 'use strict';
 
-// TODO: check require.resolve;
+var path = require('path');
+var helpers = require('./_helpers');
 
 module.exports = function(context) {
-  return {};
+  var target = context.getFilename();
+
+  var resolveOpts = {
+    basedir: path.dirname(target),
+    extensions: ['.js', '.json', '.node'],
+  };
+
+  function validate(node) {
+    var id = helpers.getModuleId(node);
+    var resolved = helpers.resolveSync(id, resolveOpts);
+    if (!resolved) {
+      context.report({
+        node: helpers.getIdNode(node),
+        data: {id: id},
+        message: '"{{id}}" does not exist.',
+      });
+    }
+  }
+
+  return {
+    CallExpression: function(node) {
+      if (helpers.isRequireCall(node) ||
+          helpers.isRequireResolveCall(node)) {
+        validate(node);
+      }
+    },
+    ImportDeclaration: function(node) {
+      if (helpers.isImport(node)) {
+        validate(node);
+      }
+    },
+    ExportAllDeclaration: function(node) {
+      if (helpers.isExportFrom(node)) {
+        validate(node);
+      }
+    },
+    ExportNamedDeclaration: function(node) {
+      if (helpers.isExportFrom(node)) {
+        validate(node);
+      }
+    },
+  };
 };
 
 module.exports.schema = [];
--- test/no-unresolved-test.js
+++ test/no-unresolved-test.js
@@ -1,1 +1,174 @@
 'use strict';
+
+var RuleTester = require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+ruleTester.run('no-unresolved', require.resolve('../no-unresolved'), {
+  valid: [
+    {
+      filename: __filename,
+      code: 'require("./resolve/foo")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/foo.js")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/foo.json")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/bar")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/bar.json")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/baz")',
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve/baz.node")',
+    },
+    {
+      filename: __filename,
+      code: 'require("resolve")',
+    },
+    {
+      filename: __filename,
+      code: 'require("resolve/package.json")',
+    },
+
+    //
+    // require.resolve, import, export
+    //
+    {
+      filename: __filename,
+      code: 'require.resolve("./resolve/foo")',
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import "./resolve/foo"',
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import bar from "./resolve/foo"',
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export * from "./resolve/foo"',
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export {bar} from "./resolve/foo"',
+    },
+  ],
+  invalid: [
+    {
+      filename: __filename,
+      code: 'require("./resolve/qux")',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("non-existing-package")',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"non-existing-package" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("resolve/non-existing-file")',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"resolve/non-existing-file" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      code: 'require("./resolve")',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve" does not exist.',
+        },
+      ],
+    },
+
+
+    //
+    // require.resolve, import, export
+    //
+    {
+      filename: __filename,
+      code: 'require.resolve("./resolve/qux")',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import "./resolve/qux"',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'import qux from "./resolve/qux"',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export {qux} from "./resolve/qux"',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+    {
+      filename: __filename,
+      parserOptions: {sourceType: 'module'},
+      code: 'export * from "./resolve/qux"',
+      errors: [
+        {
+          type: 'Literal',
+          message: '"./resolve/qux" does not exist.',
+        },
+      ],
+    },
+  ],
+});
